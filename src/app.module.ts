b9b34c719609105eedb91c8349ae17901be2f152
Jadilson Guedes <jadilson12@gmail.com>
2020-03-13 20:32:14 -0300

    chore: add ProductModule end mongooseModule database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ProductModule } from './modules/product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
- 
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/store'),
+    ProductModule
+  ]
 })
 export class AppModule {}
