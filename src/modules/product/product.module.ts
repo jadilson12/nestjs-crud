111c3e0894e4e303aaf15797117e493cf0a59fcb
Jadilson Guedes <jadilson12@gmail.com>
2020-03-13 20:26:26 -0300

    perf:  create module for product

---
+++ src/modules/product/product.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductController } from 'src/controllers/product/product.controller';
+import { ProductService } from 'src/services/product/product.service';
+import { ProductSchema } from 'src/schemas/product.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }])],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
+export class ProductModule {}
